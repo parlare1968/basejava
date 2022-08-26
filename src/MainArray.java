f8ec0b15f068c3fac9aff9df34badfc28714d9e9
dedov-aa <adedov97@mail.ru>
2022-08-26 22:20:55 +0300

    MainArray.java updated

--- src/MainArray.java
+++ src/MainArray.java
@@ -31,8 +31,7 @@ public class MainArray {
                     System.out.println(ARRAY_STORAGE.size());
                     break;
                 case "save":
-                    r = new Resume();
-                    r.uuid = uuid;
+                    r = new Resume(uuid);
                     ARRAY_STORAGE.save(r);
                     printAll();
                     break;

249e5601415fcaae952190acf1aeaaabd0d6d7a8
dedov-aa <adedov97@mail.ru>
2022-09-24 14:28:52 +0300

    MainTestArrayStorage.java updated

--- src/MainTestArrayStorage.java
+++ src/MainTestArrayStorage.java
@@ -13,13 +13,13 @@ public class MainTestArrayStorage {
         ARRAY_STORAGE.save(r2);
         ARRAY_STORAGE.save(r3);
 
-        System.out.println("Get r1: " + ARRAY_STORAGE.get(r1.getUUID()));
+        System.out.println("Get r1: " + ARRAY_STORAGE.get(r1.getUuid()));
         System.out.println("Size: " + ARRAY_STORAGE.size());
 
         System.out.println("Get dummy: " + ARRAY_STORAGE.get("dummy"));
 
         printAll();
-        ARRAY_STORAGE.delete(r1.getUUID());
+        ARRAY_STORAGE.delete(r1.getUuid());
         printAll();
         ARRAY_STORAGE.clear();
         printAll();

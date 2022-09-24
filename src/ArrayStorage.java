d9fe84f35fb2c1ac4778480d4ba3c6c323efaf90
dedov-aa <adedov97@mail.ru>
2022-09-24 20:41:27 +0300

    ArrayStorage.java updated

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -8,7 +8,7 @@ public class ArrayStorage {
     private int size = 0;
 
     public void clear() {
-        Arrays.fill(storage, null);
+        Arrays.fill(storage, 0, size, null);
         size = 0;
     }
 

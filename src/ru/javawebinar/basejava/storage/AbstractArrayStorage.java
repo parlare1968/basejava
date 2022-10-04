684b6ce06e7adda3d5ed9b0dbceb94cc3669dad5
dedov-aa <adedov97@mail.ru>
2022-10-04 22:00:51 +0300

    AbstractArrayStorage.java class added

---
+++ src/ru/javawebinar/basejava/storage/AbstractArrayStorage.java
@@ -0,0 +1,24 @@
+package ru.javawebinar.basejava.storage;
+
+import ru.javawebinar.basejava.model.Resume;
+
+public abstract class AbstractArrayStorage implements Storage {
+    protected final int STORAGE_LIMIT = 10000;
+    protected final Resume[] storage = new Resume[STORAGE_LIMIT];
+    protected int size = 0;
+
+    public int size() {
+        return size;
+    }
+
+    public final Resume get(String uuid) {
+        int index = getSearchKey(uuid);
+        if (index == -1) {
+            System.out.println("ERROR: resume with uuid = " + uuid + " is missing in the database");
+            return null;
+        }
+        return storage[index];
+    }
+
+    protected abstract int getSearchKey(String uuid);
+}

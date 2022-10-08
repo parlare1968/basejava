74c4e050258fe60cc8a8609fc039a1c95285530d
dedov-aa <adedov97@mail.ru>
2022-10-08 14:31:20 +0300

    methods clear, update, delete, getAll are moved to AbstractArrayStorage class

--- src/ru/javawebinar/basejava/storage/ArrayStorage.java
+++ src/ru/javawebinar/basejava/storage/ArrayStorage.java
@@ -2,13 +2,12 @@ package ru.javawebinar.basejava.storage;
 
 import ru.javawebinar.basejava.model.Resume;
 
-import java.util.Arrays;
-
 /**
  * Array based storage for Resumes
  */
 public class ArrayStorage extends AbstractArrayStorage {
 
+    @Override
     protected int getSearchKey(String uuid) {
         for (var i = 0; i < size; i++) {
             if (storage[i].getUuid().equals(uuid)) {
@@ -18,11 +17,7 @@ public class ArrayStorage extends AbstractArrayStorage {
         return -1;
     }
 
-    public void clear() {
-        Arrays.fill(storage, 0, size, null);
-        size = 0;
-    }
-
+    @Override
     public void save(Resume r) {
         if (size == storage.length) {
             System.out.println("ERROR: array storage is completely filled");
@@ -32,32 +27,5 @@ public class ArrayStorage extends AbstractArrayStorage {
             storage[size] = r;
             size++;
         }
-    }
-
-    public void update(Resume r) {
-        int index = getSearchKey(r.getUuid());
-        if (index == -1) {
-            System.out.println("ERROR: resume with uuid = " + r.getUuid() + " is missing in the database");
-        } else {
-            storage[index] = r;
-        }
-    }
-
-    public void delete(String uuid) {
-        int index = getSearchKey(uuid);
-        if (index == -1) {
-            System.out.println("ERROR: resume with uuid = " + uuid + " is missing in the database");
-        } else {
-            System.arraycopy(storage, index + 1, storage, index, size - index - 1);
-            size--;
-            storage[size] = null;
-        }
-    }
-
-    /**
-     * @return array, contains only Resumes in storage (without null)
-     */
-    public Resume[] getAll() {
-        return Arrays.copyOf(storage, size);
     }
 }

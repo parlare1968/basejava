e9e2ceef5e22dcde57e28c50b6f91a753bf04919
dedov-aa <adedov97@mail.ru>
2022-10-11 20:31:09 +0300

    StorageExceptions added

---
+++ src/ru/javawebinar/basejava/exception/ExistStorageException.java
@@ -0,0 +1,8 @@
+package ru.javawebinar.basejava.exception;
+
+public class ExistStorageException extends StorageException {
+    public ExistStorageException(String uuid) {
+        super("Resume with uuid = " + uuid + " is present in the database", uuid);
+    }
+
+}

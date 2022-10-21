bfe66b1433bf44b6e102f0911b51b1c7d85b8a96
dedov-aa <adedov97@mail.ru>
2022-10-21 21:38:40 +0300

    classes formatted

--- src/ru/javawebinar/basejava/exception/NotExistStorageException.java
+++ src/ru/javawebinar/basejava/exception/NotExistStorageException.java
@@ -1,6 +1,7 @@
 package ru.javawebinar.basejava.exception;
 
 public class NotExistStorageException extends StorageException {
+
     public NotExistStorageException(String uuid) {
         super("Resume with uuid = " + uuid + " is missing in the database", uuid);
     }

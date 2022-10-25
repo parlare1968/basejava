15c286dc18ce0dae54ceb8af2add8a41203868f9
dedov-aa <adedov97@mail.ru>
2022-10-25 16:10:57 +0300

    AbstractArrayStorage.java updated

--- src/ru/javawebinar/basejava/storage/AbstractArrayStorage.java
+++ src/ru/javawebinar/basejava/storage/AbstractArrayStorage.java
@@ -1,74 +1,61 @@
 package ru.javawebinar.basejava.storage;
 
-import ru.javawebinar.basejava.exception.ExistStorageException;
-import ru.javawebinar.basejava.exception.NotExistStorageException;
 import ru.javawebinar.basejava.exception.StorageException;
 import ru.javawebinar.basejava.model.Resume;
 
 import java.util.Arrays;
 
-public abstract class AbstractArrayStorage implements Storage {
+public abstract class AbstractArrayStorage extends AbstractStorage {
     protected static final int STORAGE_LIMIT = 10000;
+
     protected final Resume[] storage = new Resume[STORAGE_LIMIT];
     protected int size = 0;
 
+    @Override
     public void clear() {
         Arrays.fill(storage, 0, size, null);
         size = 0;
     }
 
-    public final void save(Resume r) {
-        int searchKey = getSearchKey(r.getUuid());
+    /**
+     * @return array, contains only Resumes in storage (without null)
+     */
+    @Override
+    public Resume[] getAll() {
+        return Arrays.copyOf(storage, size);
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    protected void saveResume(Resume r, int searchKey) {
         if (size == STORAGE_LIMIT) {
             throw new StorageException("Array storage is completely filled", r.getUuid());
-        } else if (searchKey >= 0) {
-            throw new ExistStorageException(r.getUuid());
         } else {
             insertResume(r,searchKey);
             size++;
         }
     }
 
-    public final void update(Resume r) {
-        int index = getSearchKey(r.getUuid());
-        if (index < 0) {
-            throw new NotExistStorageException(r.getUuid());
-        } else {
-            storage[index] = r;
-        }
+    @Override
+    protected void updateResume(Resume r, int searchKey) {
+        storage[searchKey] = r;
     }
 
-    public final Resume get(String uuid) {
-        int index = getSearchKey(uuid);
-        if (index < 0) {
-            throw new NotExistStorageException(uuid);
-        }
-        return storage[index];
+    @Override
+    protected Resume getResume(int searchKey) {
+        return storage[searchKey];
     }
 
-    public final void delete(String uuid) {
-        int index = getSearchKey(uuid);
-        if (index < 0) {
-            throw new NotExistStorageException(uuid);
-        } else {
-            removeResume(index);
-            storage[size - 1] = null;
-            size--;
-        }
+    @Override
+    protected void deleteResume(int searchKey) {
+        removeResume(searchKey);
+        storage[size - 1] = null;
+        size--;
     }
-
-    /**
-     * @return array, contains only Resumes in storage (without null)
-     */
-    public Resume[] getAll() {
-        return Arrays.copyOf(storage, size);
-    }
-
-    public int size() {
-        return size;
-    }
-
-    protected abstract int getSearchKey(String uuid);
 
     protected abstract void removeResume(int deletedElementIndex);
 

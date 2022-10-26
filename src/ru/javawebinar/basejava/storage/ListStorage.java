44d78bc1413a25d2f346bd0a719095b90531398f
dedov-aa <adedov97@mail.ru>
2022-10-26 23:47:07 +0300

    list storage made final

--- src/ru/javawebinar/basejava/storage/ListStorage.java
+++ src/ru/javawebinar/basejava/storage/ListStorage.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ListStorage extends AbstractStorage {
-    List<Resume> storage = new ArrayList<>();
+    private final List<Resume> storage = new ArrayList<>();
 
     @Override
     protected boolean isExist(Object searchKey) {

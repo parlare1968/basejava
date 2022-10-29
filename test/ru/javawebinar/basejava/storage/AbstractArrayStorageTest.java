144afb5821388215a3851dced96626e86a257ba6
dedov-aa <adedov97@mail.ru>
2022-10-29 23:13:37 +0300

    saveOverflow move to AbstractArrayStorageTest.java

---
+++ test/ru/javawebinar/basejava/storage/AbstractArrayStorageTest.java
@@ -0,0 +1,26 @@
+package ru.javawebinar.basejava.storage;
+
+import org.junit.Assert;
+import org.junit.Test;
+import ru.javawebinar.basejava.exception.StorageException;
+import ru.javawebinar.basejava.model.Resume;
+
+public abstract class AbstractArrayStorageTest extends AbstractStorageTest {
+
+    protected AbstractArrayStorageTest(Storage storage) {
+        super(storage);
+    }
+
+    @Test(expected = StorageException.class)
+    public void saveOverflow() {
+        storage.clear();
+        try {
+            for (int i = 0; i < AbstractArrayStorage.STORAGE_LIMIT; i++) {
+                storage.save(new Resume("fullName" + i));
+            }
+        } catch (StorageException e) {
+            Assert.fail("Overflow occurred ahead of time");
+        }
+        storage.save(new Resume("overflow"));
+    }
+}

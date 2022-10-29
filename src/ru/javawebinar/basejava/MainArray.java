d848a16ab0d65aab4e1d3a12ea8496658922119b
dedov-aa <adedov97@mail.ru>
2022-10-30 00:06:35 +0300

    MainArray.java updated

--- src/ru/javawebinar/basejava/MainArray.java
+++ src/ru/javawebinar/basejava/MainArray.java
@@ -19,15 +19,20 @@ public class MainArray {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         Resume r;
         while (true) {
-            System.out.print("Введите одну из команд - (list | size | save uuid | update uuid | delete uuid | get uuid | clear | exit): ");
+            System.out.print("Введите одну из команд - (list | size | save fullName uuid | update fullName uuid | delete uuid | get uuid | clear | exit): ");
             String[] params = reader.readLine().trim().toLowerCase().split(" ");
-            if (params.length < 1 || params.length > 2) {
+            if (params.length < 1 || params.length > 3) {
                 System.out.println("Неверная команда.");
                 continue;
             }
             String uuid = null;
+            String fullName = null;
             if (params.length == 2) {
-                uuid = params[1].intern();
+                fullName = params[1].intern();
+            }
+            if (params.length == 3) {
+                fullName = params[1].intern();
+                uuid = params[2].intern();
             }
             switch (params[0]) {
                 case "list":
@@ -37,12 +42,12 @@ public class MainArray {
                     System.out.println(ARRAY_STORAGE.size());
                     break;
                 case "save":
-                    r = new Resume(uuid);
+                    r = uuid == null ? new Resume(fullName) : new Resume(fullName, uuid);
                     ARRAY_STORAGE.save(r);
                     printAll();
                     break;
                 case "update":
-                    r = new Resume(uuid);
+                    r = uuid == null ? new Resume(fullName) : new Resume(fullName, uuid);
                     ARRAY_STORAGE.update(r);
                     printAll();
                     break;

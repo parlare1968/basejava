9539bb6241674202b973bdd74c078b95fa1e9caa
dedov-aa <adedov97@mail.ru>
2022-10-30 14:05:46 +0300

    MapUuidStorageTest updated

--- test/ru/javawebinar/basejava/storage/MapUuidStorageTest.java
+++ test/ru/javawebinar/basejava/storage/MapUuidStorageTest.java
@@ -3,6 +3,6 @@ package ru.javawebinar.basejava.storage;
 public class MapUuidStorageTest extends AbstractStorageTest {
 
     public MapUuidStorageTest() {
-        super(new MapStorage());
+        super(new MapUuidStorage());
     }
 }

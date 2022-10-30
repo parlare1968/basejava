f3d947c5cef0dd51630967aac3c92a57a74b3b18
dedov-aa <adedov97@mail.ru>
2022-10-30 14:38:16 +0300

    classes added

---
+++ src/ru/javawebinar/basejava/storage/AbstractMapStorage.java
@@ -0,0 +1,42 @@
+package ru.javawebinar.basejava.storage;
+
+import ru.javawebinar.basejava.model.Resume;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public abstract class AbstractMapStorage extends AbstractStorage {
+    protected final Map<String, Resume> map = new HashMap<>();
+
+    @Override
+    public void clear() {
+        map.clear();
+    }
+
+    @Override
+    public int size() {
+        return map.size();
+    }
+
+    @Override
+    protected void doSave(Resume r, Object searchKey) {
+        map.put(r.getUuid(), r);
+    }
+
+    @Override
+    protected void doUpdate(Resume r, Object searchKey) {
+        map.put(r.getUuid(), r);
+    }
+
+    @Override
+    protected boolean isExist(Object searchKey) {
+        return searchKey != null;
+    }
+
+    @Override
+    protected List<Resume> copyToList() {
+        return new ArrayList<>(map.values());
+    }
+}

d322ec009b6fcbf2197c45b54698338c2ffb4107
dedov-aa <adedov97@mail.ru>
2022-10-30 14:39:15 +0300

    MapUuidStorage updated

--- src/ru/javawebinar/basejava/storage/MapUuidStorage.java
+++ src/ru/javawebinar/basejava/storage/MapUuidStorage.java
@@ -2,23 +2,7 @@ package ru.javawebinar.basejava.storage;
 
 import ru.javawebinar.basejava.model.Resume;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-public class MapUuidStorage extends AbstractStorage {
-    private final Map<String, Resume> map = new HashMap<>();
-
-    @Override
-    public void clear() {
-        map.clear();
-    }
-
-    @Override
-    public int size() {
-        return map.size();
-    }
+public class MapUuidStorage extends AbstractMapStorage {
 
     @Override
     protected Object getSearchKey(String uuid) {
@@ -26,32 +10,12 @@ public class MapUuidStorage extends AbstractStorage {
     }
 
     @Override
-    protected void doSave(Resume r, Object searchKey) {
-        map.put(r.getUuid(), r);
+    protected Resume doGet(Object uuid) {
+        return map.get((String) uuid);
     }
 
     @Override
-    protected void doUpdate(Resume r, Object searchKey) {
-        map.put(r.getUuid(), r);
-    }
-
-    @Override
-    protected Resume doGet(Object searchKey) {
-        return map.get((String) searchKey);
-    }
-
-    @Override
-    protected void doDelete(Object searchKey) {
-        map.remove((String) searchKey);
-    }
-
-    @Override
-    protected boolean isExist(Object searchKey) {
-        return searchKey != null;
-    }
-
-    @Override
-    protected List<Resume> copyToList() {
-        return new ArrayList<>(map.values());
+    protected void doDelete(Object uuid) {
+        map.remove((String) uuid);
     }
 }

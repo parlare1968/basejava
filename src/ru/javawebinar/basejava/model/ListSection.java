4d609900626e8312f76cfdc5c4fa7074187b7f8e
dedov-aa <adedov97@mail.ru>
2022-11-04 23:49:42 +0300

    TextSection.java and ListSection.java updated

---
+++ src/ru/javawebinar/basejava/model/ListSection.java
@@ -0,0 +1,40 @@
+package ru.javawebinar.basejava.model;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ListSection extends Section {
+    List<String> list;
+
+    public ListSection(List<String> list) {
+        this.list = list;
+    }
+
+    public ListSection(String... list) {
+        this.list = Arrays.asList(list);
+    }
+
+    public List<String> getList() {
+        return list;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        ListSection that = (ListSection) o;
+
+        return list.equals(that.list);
+    }
+
+    @Override
+    public int hashCode() {
+        return list.hashCode();
+    }
+
+    @Override
+    public String toString() {
+        return list.toString();
+    }
+}

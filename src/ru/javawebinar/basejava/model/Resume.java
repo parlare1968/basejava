f452a0aa4fdad4e6aa3a44e9bccf24c74fc1976e
dedov-aa <adedov97@mail.ru>
2022-11-04 23:41:37 +0300

    Resume.java updated

--- src/ru/javawebinar/basejava/model/Resume.java
+++ src/ru/javawebinar/basejava/model/Resume.java
@@ -1,12 +1,18 @@
 package ru.javawebinar.basejava.model;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.UUID;
 
 public class Resume {
-
     // Unique identifier
     private final String uuid;
+
     private final String fullName;
+
+    private final Map<ContactType, String> contacts = new HashMap<>();
+
+    private final Map<SectionType, Section> sections = new HashMap<>();
 
     public Resume(String fullName) {
         this(UUID.randomUUID().toString(), fullName);
@@ -23,6 +29,22 @@ public class Resume {
 
     public String getFullName() {
         return fullName;
+    }
+
+    public String getContact(ContactType type) {
+        return contacts.get(type);
+    }
+
+    public Section getSection(SectionType type) {
+        return sections.get(type);
+    }
+
+    public void addContact(ContactType type, String contact) {
+        contacts.put(type, contact);
+    }
+
+    public void addSection(SectionType type, Section section) {
+        sections.put(type, section);
     }
 
     @Override

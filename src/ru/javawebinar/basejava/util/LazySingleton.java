0ecc3708e426a8b16dd8e89ca162c09d04d5174b
dedov-aa <adedov97@mail.ru>
2022-12-11 14:32:47 +0300

    LazySingleton added

---
+++ src/ru/javawebinar/basejava/util/LazySingleton.java
@@ -0,0 +1,27 @@
+package ru.javawebinar.basejava.util;
+
+public class LazySingleton {
+    //int i;
+    volatile private static LazySingleton INSTANCE;
+
+    //double sin = Math.sin(13.);
+
+    private LazySingleton() {
+    }
+
+    private static class LazySingletonHolder {
+        static final LazySingleton INSTANCE = new LazySingleton();
+    }
+
+    public static LazySingleton getInstance() {
+        return LazySingletonHolder.INSTANCE;
+        //if (INSTANCE == null) {
+        //    synchronized (LazySingleton.class) {
+        //        if (INSTANCE == null) {
+        //            INSTANCE = new LazySingleton();
+        //        }
+        //    }
+        //}
+        //return INSTANCE;
+    }
+}

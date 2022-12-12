fa74c355523f85a36e1690f81f2dac85bd4435e8
dedov-aa <adedov97@mail.ru>
2022-12-12 22:47:43 +0300

    HW12 MainStream.java class added

---
+++ src/ru/javawebinar/basejava/MainStream.java
@@ -0,0 +1,36 @@
+package ru.javawebinar.basejava;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class MainStream {
+    public static void main(String[] args) {
+        int[] array = {1, 2, 2, 3, 4, 7, 5, 7};
+        System.out.println(minValue(array));
+        //
+        List<Integer> list = Arrays.asList(1, 2, 4);
+        System.out.println(oddOrEven(list));
+    }
+
+    public static int minValue(int[] values) {
+        return Arrays.stream(values)
+                .distinct()
+                .sorted()
+                .reduce((a,b)->a * 10 + b)
+                .getAsInt();
+    }
+
+    public static List<Integer> oddOrEven(List<Integer> integers) {
+        // Используем свойства четности целых чисел:
+        // 1) Cумма четных чисел четна
+        // 2) Сумма четного и нечетного чисел нечетна
+        // 3) Сумма четного количества нечетных чисел четно
+        // 4) Сумма нечетного количества нечетных чисел нечетна
+        Map<Boolean, List<Integer>> map = integers.stream().collect(Collectors.partitioningBy(n -> n % 2 == 0));
+        // true - массив четных чисел (сумма четна)
+        // false - массив нечетных чисел
+        return map.get(map.get(false).size() % 2 != 0);
+    }
+}

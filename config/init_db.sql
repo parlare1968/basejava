620d9f05bcfac2f9e97dbeb4d55e4cbd0e560311
dedov-aa <adedov97@mail.ru>
2022-12-16 23:58:06 +0300

    init_db.sql added

---
+++ config/init_db.sql
@@ -0,0 +1,18 @@
+create table resume
+(
+    uuid      char(36) not null
+        primary key,
+    full_name text
+);
+
+create table contact
+(
+    id          serial
+        primary key,
+    type        text     not null,
+    value       text     not null,
+    resume_uuid char(36) not null
+        constraint contact_resume_uuid_fk
+            references resume
+            on delete cascade
+);

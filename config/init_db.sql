3550d11cb5aff10ad79f2ad0fa8174e8d6079613
dedov-aa <adedov97@mail.ru>
2022-12-17 00:07:39 +0300

    init_db.sql updated

--- config/init_db.sql
+++ config/init_db.sql
@@ -1,18 +1,17 @@
 create table resume
 (
-    uuid      char(36) not null
-        primary key,
-    full_name text
+    uuid      char(36) not null primary key,
+    full_name text     not null
 );
 
 create table contact
 (
-    id          serial
-        primary key,
-    type        text     not null,
-    value       text     not null,
+    id          serial primary key,
     resume_uuid char(36) not null
-        constraint contact_resume_uuid_fk
-            references resume
-            on delete cascade
+        constraint contact_resume_uuid_fk references resume on delete cascade,
+    type        text     not null,
+    value       text     not null
 );
+
+create unique index contact_uuid_type_index
+    on contact (resume_uuid, type);
